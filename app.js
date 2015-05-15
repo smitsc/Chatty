1b6c64dd191158ed98ff278fb5fb4f51534e3efc
smitsc <scott@virtualbs.com>
2015-05-14 18:36:17 -0700

    2nd commit
    stuff

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){ 
    console.log( 'a user connected' ); 
  
    socket.on('chat message', function(msg){ 
     io.emit('chat message', msg); 
-	console.log('gettinh here');
+	console.log('getting here ' + msg);
    }); 
  }); 
 
